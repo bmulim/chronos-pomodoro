121daf37e752136ce89566443b88245ae948dd0d
Bruno Mulim <108417283+bmulim@users.noreply.github.com>
2025-09-21 20:18:11 -0300

    Conclusão de mais algumas auas

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -4,9 +4,20 @@ import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
 import { useRef } from 'react';
 import type { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { getNextCycle } from '../../utils/getNextCycle';
+import { getNextCycleType } from '../../utils/getNextCycleType';
+import { formatSecondsToMinutes } from '../../utils/formatSecondsToMinutes';
 
 export function MainForm() {
+  const { state, setState } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
+
+  //ciclos
+  const nextCycle = getNextCycle(state.currentCycle);
+
+  //tipo de ciclo
+  const nextCycleType = getNextCycleType(nextCycle);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -15,14 +26,34 @@ export function MainForm() {
 
     const taskName = taskNameInput.current.value.trim();
 
-    if(!taskName){
-        alert('Digite uma tarefa!');
-        return;
+    if (!taskName) {
+      alert('Digite uma tarefa!');
+      return;
     }
 
     const newTask: TaskModel = {
-        
-    }
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      durationInMinutes: state.config[nextCycleType],
+      type: nextCycleType,
+    };
+
+    const secondsRemaining = newTask.durationInMinutes * 60;
+
+    setState((prevState) => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining,
+        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
   }
 
   return (
@@ -34,16 +65,21 @@ export function MainForm() {
           labelText={`Task`}
           placeholder="Digite sua tarefa"
           ref={taskNameInput}
+          disabled={!!state.activeTask}
         />
       </div>
 
-      <div className="formRow">
-        <p>Próximo intervalo: 25 min</p>
-      </div>
+      {state.currentCycle > 0 && (
+        <>
+          <div className="formRow">
+            <p>Próximo intervalo: 25 min</p>
+          </div>
 
-      <div className="formRow">
-        <Cycles />
-      </div>
+          <div className="formRow">
+            <Cycles />
+          </div>
+        </>
+      )}
 
       <div className="formRow">
         <DefaultButton icon={<PlayCircleIcon />} color="green" />
